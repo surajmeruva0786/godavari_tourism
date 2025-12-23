2f4b64dde5b98ba582576288f3a0df678cb1d6d4
surajmeruva0786 <meruva24102@iiitnr.edu.in>
2025-12-23 13:15:28 +0530

    update db security

--- src/app/config/firebase.config.ts
+++ src/app/config/firebase.config.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBCHTqB3DORVk2O2jPgTU7BxnrlCJz5__M",
-    authDomain: "godavaritourism-bec1f.firebaseapp.com",
-    projectId: "godavaritourism-bec1f",
-    storageBucket: "godavaritourism-bec1f.firebasestorage.app",
-    messagingSenderId: "550352672428",
-    appId: "1:550352672428:web:5be4d7bba3a7699d07b109",
-    measurementId: "G-L4NZ3MY80B"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
